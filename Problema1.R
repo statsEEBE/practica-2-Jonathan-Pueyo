237fe233f3088d63e57ba8e762dc3e8aa3c45ed0
jonathan.pueyo <jonathan.pueyo@auleseebe.upc.edu>
2025-02-25 09:55:25 +0100

    solucion problema 1

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,18 @@
 #Codigo para problema 1
+
+# coeficient de determiancio => R^2
+Rsq <- 0.1923
+# coeficint de correlacio (de pearson) => sqrt(R^2)
+sqrt(0.1923)
+# por una unidad de aumento en assets (x), variacion de mercat value (y)
+m <- 0.06576
+# definimos la recta ypred = x*m + b
+b <- 11.79755
+prediccion <- 647.1*m+b
+
+
+#cargar datos de un fichero de texto
+data <- read.delim("air.txt", dec=",", sep="\t")
+data
+
+data <- read.table("air.txt", dec=",", header=TRUE, sep=" ")
